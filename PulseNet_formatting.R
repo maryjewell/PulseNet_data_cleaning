188b37a6cebafcfee85c4688254241d2dc69bace
Mary Jewell <mjewell@utah.gov>
2024-10-01 12:04:14 -0600

    Add files via upload

---
+++ PulseNet_formatting.R
@@ -0,0 +1,83 @@
+# Author: Mary Jewell
+# Date created: 9/11/2024
+# Last updated: 9/30/2024
+# Notes: Format PulseNet 2.0 data from LIMS export
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## Load packages
+rm(list = ls())
+pacman::p_load(
+  tidyverse,  # data manipulation
+  janitor,    # clean column names
+  readxl,     # read and write Excel files
+  writexl,    # write Excel files
+  lubridate   # date formatting
+)
+
+## Read data
+# Replace these file paths with the correct paths on your computer
+# Important note: all slashes in file path should be / forward slashes!
+lims <- read_excel("C:/Jenni R Scripts/Beginning data.xls", col_names = T, skip = 1)
+lims <- janitor::clean_names(lims)
+
+## Create/clean variables
+# Key
+lims$Key <- lims$samp_number
+
+# Location
+lims$SourceCountry <- lims$country
+lims$SourceState <- "UT"
+lims$SourceType <- "Human"
+
+# Format age variables
+lims$PatientAgeDays	<- ""
+lims$PatientAgeMonths	<- ifelse(grepl("mo", lims$age), lims$age, "")
+lims$PatientAgeMonths <- gsub("mo", "", lims$PatientAgeMonths)
+lims$PatientAgeYears <- ifelse(grepl("mo", lims$age) == F, lims$age, "")
+
+# Patient sex
+lims$PatientSex <- ifelse(lims$sex == "M", "MALE", "FEMALE")
+
+# Source site
+lims$SourceSite <- str_to_title(lims$specimen_source)
+
+# Date variables
+# Isolation Date
+lims$date_collected <- as.character(lims$date_collected)
+lims$IsolatDate <- sub(" \\d{2}:\\d{2}:\\d{2}$", "", lims$date_collected)
+lims$IsolatDate <- format(ymd(lims$IsolatDate), "%Y-%m-%d")
+# Received Date
+lims$date_received <- as.character(lims$date_received)
+lims$ReceivedDate <- sub(" \\d{2}:\\d{2}:\\d{2}$", "", lims$date_received)
+lims$ReceivedDate <- format(ymd(lims$ReceivedDate), "%m/%d/%Y")
+# Lab Received Date?
+lims$WGS_Lab_ReceivedDate <- ""
+
+# Genus
+lims <- mutate(lims,
+               Genus = case_when(
+                 bacteriology_result == "CAMPYLOBACTER jejuni" ~ "jejuni",
+                 bacteriology_result == "ESCHERICHIA coli, O157:H7" ~ "E. coli O157",
+                 bacteriology_result == "SHIGELLA flexneri" ~ "SHIGELLA flexneri",
+                 bacteriology_result == "SHIGELLA sonnei" ~ "SHIGELLA sonnei"
+               ))
+
+
+# Sequencer Run ID
+lims$SequencerRun_id <- ""
+
+
+
+## Select columns
+cleaned_data <- lims %>% select(Key, SourceCountry, SourceState, SourceType,	
+                                PatientAgeDays, PatientAgeMonths, PatientAgeYears,
+                                PatientSex, SourceSite, IsolatDate, ReceivedDate,
+                                WGS_Lab_ReceivedDate,	Genus, SequencerRun_id)
+
+# Remove footer
+cleaned_data <- cleaned_data %>% filter(Key != "UT_Folder_Summary_Test_PFGE.rpt")
+
+
+## Write finished data
+write_xlsx(cleaned_data, "C:/Jenni R Scripts/Jenni-end-data-test.xlsx")
+
---
+++ PulseNet_merge.R
@@ -0,0 +1,47 @@
+# Author: Mary Jewell
+# Date created: 9/11/2024
+# Last updated: 9/25/2024
+# Notes: Merge PulseNet 2.0 data with PHI
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## Load packages
+# install.packages("pacman")
+pacman::p_load(
+  tidyverse, # data manipulation
+  janitor,   # clean column names
+  openxlsx,   # read and write Excel files
+  readxl
+)
+
+
+## Read data
+# Replace these file paths with the correct paths on your computer
+# Important note: all slashes in file path should be / forward slashes!
+pulsenet <- read_excel("G:/MICRO/MOLECULAR LABORATORY/PFGEProgram/WGS/PulseNet 2.0/Export report from PN 2.0/Campy 9.20.24.xlsx")
+phi <- read.csv("L:/Shared_Files/Mirth/COVID_data_import/Ent_Summary_09_20_2024.csv")
+
+## Clean column names
+pulsenet <- janitor::clean_names(pulsenet)
+phi <- janitor::clean_names(phi)
+
+## Select relevant column names from Epi NGS Pending List
+phi <- phi %>% select(sample_number, last_name, first_name, dob, customer_name)
+
+## Merge data
+merged_data <- merge(pulsenet, phi,
+                     by.x = c("key"),
+                     by.y = c("sample_number"),
+                     all.x = T)
+
+
+# Remove columns with 100% missingness
+merged_data <- merged_data %>% purrr::discard(~sum(is.na(.x))/length(.x)*100 == 100)
+
+# Formatting dates
+merged_data$dob <- as.Date(merged_data$dob,
+                           format = "%m/%d/%y")
+
+
+## Write out data
+# Change file name!
+write.xlsx(merged_data, "G:/MICRO/MOLECULAR LABORATORY/PFGEProgram/WGS/Epi reports/2024/campyv3.xlsx")
